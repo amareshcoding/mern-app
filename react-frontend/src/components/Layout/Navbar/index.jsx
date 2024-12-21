cb3f06966845d9dd3ef24a9f0c3489e8b0b6622b
Amaresh Barik <amaresh.acb@gmail.com>
2024-12-21 17:39:08 +0530

    navbar layout

--- react-frontend/src/components/Layout/Navbar/index.jsx
+++ react-frontend/src/components/Layout/Navbar/index.jsx
@@ -1,10 +1,28 @@
-
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <nav className="fixed top-0 left-0 w-full bg-white text-gray-600 py-4 px-8 shadow-lg">
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-bold">My Navbar</h1>
+        <ul className="flex space-x-6">
+          <li>
+            <a href="#home" className="hover:text-gray-300">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="hover:text-gray-300">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="hover:text-gray-300">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
